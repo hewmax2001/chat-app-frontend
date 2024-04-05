be7e7f696fc368b92260cbce4a6b01858b31b544
Max Hewitt <mxhew20@gmail.com>
2024-04-05 19:22:35 +1300

    Implement login form component
    LoginForm functionality now dispatches loginUser action and navigates to '/'

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,37 +1,49 @@
 import { useState } from 'react'
 import './login.css'
+import { useDispatch } from 'react-redux'
+import { loginUser } from '../reducers/userReducer'
+import { useNavigate } from 'react-router-dom'
 
 const LoginForm = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-    // Replace with textField hook?
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+  // Replace with textField hook?
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
-    const handleLogin = (event) => {
-        event.preventDefault()
-        console.log(username, password)
-    }
+  const handleLogin = async (event) => {
+      event.preventDefault()
+      try {
+        await dispatch(loginUser(username, password))
+        setUsername('')
+        setPassword('')
+        navigate('/')
+      } catch(error) {
+        console.error(`Error occured: ${error}`)
+      }
+  }
 
-    return (
-      <div className='center-wrapper'>
-        <form className='login-form' onSubmit={handleLogin}>
-          <div>
-            <p>Username</p>
-            <input type='text' name='username' value={username} 
-            onChange={({target}) => setUsername(target.value)}/>
-          </div>
-          <div>
-            <p>Password</p>
-            <input type='password' name='password' value={password}
-            onChange={({target}) => setPassword(target.value)}/>
-          </div>
-          <div>
-            <button className='login-button' type='submit'>Login</button>
-          </div>
-          <p>Don't have an account? <a href='register'>register</a></p>
-        </form>
-      </div>
-    )
+  return (
+    <div className='center-wrapper'>
+      <form className='login-form' onSubmit={handleLogin}>
+        <div>
+          <p>Username</p>
+          <input type='text' name='username' value={username} 
+          onChange={({target}) => setUsername(target.value)}/>
+        </div>
+        <div>
+          <p>Password</p>
+          <input type='password' name='password' value={password}
+          onChange={({target}) => setPassword(target.value)}/>
+        </div>
+        <div>
+          <button className='login-button' type='submit'>Login</button>
+        </div>
+        <p>Don't have an account? <a href='register'>register</a></p>
+      </form>
+    </div>
+  )
 }
 
 export default LoginForm
