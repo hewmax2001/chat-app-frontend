16af48cceaee6a8545845bdf98de5f81dad34f23
Max Hewitt <mxhew20@gmail.com>
2024-04-13 18:17:03 +1200

    Refractor store import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import store from './store.js'
+import store from './app/store.js'
 import { BrowserRouter as Router } from "react-router-dom"
 import { Provider } from 'react-redux'
 
